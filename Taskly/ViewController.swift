821a20655aef8ffb5e42db51c455396f38a17eef
KhaledAbouSeada <60839946+KhaledAbouSeada@users.noreply.github.com>
2020-05-15 14:24:38 +0200

    Initial Commit

---
+++ Taskly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taskly
+//
+//  Created by khaled mohamed on 5/15/20.
+//  Copyright © 2020 khaled mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
